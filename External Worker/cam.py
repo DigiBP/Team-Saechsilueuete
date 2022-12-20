42b4e030cc6f448ebe922759d9d5b8d25d1d567a
schwasa <102740850+schwasa@users.noreply.github.com>
2022-12-20 10:29:26 +0100

    Add files via upload

---
+++ External Worker/cam.py
@@ -0,0 +1,154 @@
+import requests
+import json
+import time
+import threading
+import uuid
+
+
+class Client:
+    def __init__(self, url, workerid=None):
+        self.url = url
+        if workerid:
+            self.workerid = str(workerid)
+        else:
+            self.workerid = str(uuid.uuid1())
+        self.threads = []
+        self.stop_event = threading.Event()
+
+    def __fetch_and_lock(self, endpoint, task, callback=None, interval=300):
+        try:
+            while not self.stop_event.isSet():
+                print("polling subscription: " + task["topics"][0]["topicName"])
+                response = requests.post(endpoint, json=task)
+                print(response.status_code)
+                response = response.text
+                if response != '[]':
+                    response = json.loads(response)
+                    taskid = str(response[0]['id'])
+                    if callback:
+                        callback(taskid, response)
+                    else:
+                        return response
+                else:
+                    time.sleep(interval / 1000)
+
+        except:
+            print("fail - subscription cancelled: " + task["topics"][0]["topicName"])
+
+    def subscribe(self, topic, callback=None, tenantId=None, lockDuration=20000, longPolling=29000):
+        endpoint = str(self.url) + "/external-task/fetchAndLock"
+
+        task = {"workerId": self.workerid,
+                "maxTasks": 1,
+                "usePriority": "true",
+                "asyncResponseTimeout": longPolling,
+                "topics":
+                    [{"topicName": topic,
+                        "lockDuration": lockDuration,
+                        "tenantIdIn" if tenantId else None: [tenantId]
+                        }]
+                }
+
+        if callback:
+            self.threads.append(threading.Thread(target=self.__fetch_and_lock, args=(endpoint, task, callback,)))
+        else:
+            return self.__fetch_and_lock(endpoint, task)
+
+    def polling(self):
+        try:
+            for thread in self.threads:
+                if not self.stop_event.isSet():
+                    thread.start()
+            for thread in self.threads:
+                if thread.is_alive():
+                    thread.join()
+                if self.stop_event.isSet():
+                    self.threads = []
+                    self.stop_event.clear()
+                    print("stopped - you may need to subscribe again")
+        except KeyboardInterrupt:
+            self.stop_event.set()
+
+    # Complete Command
+    def complete(self, taskid, **kwargs):
+        endpoint = str(self.url) + "/external-task/" + taskid + "/complete"
+
+        # puts the variables from the dictonary into the nested format for the json response
+        variables_for_response = {}
+        for key, val in kwargs.items():
+            variable_new = {key: {"value": val}}
+            variables_for_response.update(variable_new)
+
+        request = {
+            "workerId": self.workerid,
+            "variables": variables_for_response
+        }
+
+        try:
+            response = requests.post(endpoint, json=request)
+            body_complete = response.text
+            print(body_complete)
+            print(response.status_code)
+
+        except:
+            print('fail')
+
+    # BPMN Error Command
+    def error(self, taskid, error_code, error_message="not defined", **kwargs):
+        endpoint = str(self.url) + "/external-task/" + taskid + "/bpmnError"
+        
+        # puts the variables from the dictonary into the nested format for the json response
+        variables_for_response = {}
+        for key, val in kwargs.items():
+            variable_new = {key: {"value": val}}
+            variables_for_response.update(variable_new)
+
+        request = {
+            "workerId": self.workerid,
+            "errorCode": error_code,
+            "errorMessage": error_message,
+            "variables": variables_for_response
+        }
+
+        try:
+            response = requests.post(endpoint, json=request)
+            body_complete = response.text
+            print(body_complete)
+            print(response.status_code)
+
+        except:
+            print('fail')
+
+    # Failure Command
+    def failure(self, taskid, error_message="not defined", retries=0, retry_timeout=0):
+        endpoint = str(self.url) + "/external-task/" + taskid + "/failure"
+
+        request = {
+            "workerId": self.workerid,
+            "errorMessage": error_message,
+            "retries": retries,
+            "retryTimeout": retry_timeout
+        }
+
+        try:
+            response = requests.post(endpoint, json=request)
+            print(response.status_code)
+
+        except:
+            print('fail')
+
+    # Extend Lock
+    def extend_lock(self, taskid, new_duration):
+        endpoint = str(self.url) + "/external-task/" + taskid + "/extendLock"
+
+        request = {
+            "workerId": self.workerid,
+            "newDuration": new_duration
+        }
+
+        try:
+            response = requests.post(endpoint, json=request)
+            print(response.status_code)
+            print(self.workerid)
+        except:
+            print('fail')
---
+++ External Worker/main.py
@@ -0,0 +1,66 @@
+import cam
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import docx2txt
+import gdown
+
+class GetScoreClient:
+    def __init__(self):
+        self.worker = cam.Client("https://digibp.herokuapp.com/engine-rest")
+        self.worker.subscribe("GetScore2", self.get_score_callback, "saechsiluute")
+        self.worker.polling()
+
+    def get_score_callback(self, taskid, response):
+        '''First get the variables from the process'''
+        try:
+            doc_url = response[0]['variables']['doc_url']['value']
+        except:
+            doc_url = False
+
+        try:
+            swissmedic_id = response[0]['variables']['swissmedic_id']['value']
+        except:
+            swissmedic_id = False
+
+        try:
+            case_id = response[0]['variables']['case_id']['value']
+        except:
+            case_id = False
+
+        '''Download the document to local storage to access properly'''
+        output = "CaseID_{0}_SwissMedicID_{1}.docx".format(case_id, swissmedic_id)
+        gdown.download(doc_url, output, quiet=False, fuzzy=True)
+
+        '''Document comparing based on Tfidf'''
+
+        base_document = docx2txt.process(output)
+        documents = [docx2txt.process("Benchmark.docx")]
+
+        vectorizer = TfidfVectorizer()
+
+        # To make uniformed vectors, both documents need to be combined first.
+        documents.insert(0, base_document)
+        embeddings = vectorizer.fit_transform(documents)
+
+        cosine_similarities = cosine_similarity(embeddings[0:1], embeddings[1:]).flatten()
+
+        highest_score = 0
+        highest_score_index = 0
+        for i, score in enumerate(cosine_similarities):
+            if highest_score < score:
+                highest_score = score
+                highest_score_index = i
+
+        comp_score = highest_score
+        doc_check_message = 'Document was successfully checked'
+
+        if doc_url == False:
+            comp_score = 0
+            doc_check_message = 'doc_url is missing'
+
+        variables = {"comp_score": comp_score, "doc_check_message": doc_check_message}
+        self.worker.complete(taskid, **variables)
+
+
+if __name__ == '__main__':
+    GetScoreClient()
